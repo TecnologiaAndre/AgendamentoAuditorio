a8ca35342a25680f0be69119f22239c5d836ad4c
André Guimarães <tecnologia.andre@gertaxi.com.br>
2026-07-20 22:45:07 -0300

    Change sidebar button text to uppercase

--- main.py
+++ main.py
@@ -284,7 +284,7 @@ else:
     st.sidebar.markdown(f"**Usuário:**\n### 👤 {nome_exibicao}")
     st.sidebar.caption(f"E-mail de login: `{st.session_state.user.email}`")
     
-    if st.sidebar.button("🏃 Sair", use_container_width=True):
+    if st.sidebar.button("SAIR", use_container_width=True):
         supabase.auth.sign_out()
         st.session_state.user = None
         st.rerun()
